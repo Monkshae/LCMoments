1926112287b2c3601b82de203751060ca837cc22
licong <licong@geetest.com>
2020-03-02 19:33:03 +0800

    初始化

---
+++ LCMoments/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  LCMoments
+//
+//  Created by Richard on 2020/3/2.
+//  Copyright © 2020 Richard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ LCMoments/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  LCMoments
+//
+//  Created by Richard on 2020/3/2.
+//  Copyright © 2020 Richard. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+
+
+
+@end
